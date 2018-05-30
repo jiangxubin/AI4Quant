30d42bed81aef173a7e0eafad81ca72e036cc9c7
jiangxubin <21612149@zju.edu.cn>
2018-05-30 17:31:53 +0800

    Finish tensorflow/Keras model for Recurrent Classification

--- Ai4Quant/BaseAiModel/TimeSelection/Recurrent4Time-Tensorflow-Origin.py
+++ Ai4Quant/BaseAiModel/TimeSelection/Recurrent4Time-Tensorflow-Origin.py
@@ -17,16 +17,16 @@ batch_size = 16
 # batch_size = args.batch_size
 lstm_time_step = 10
 lstm_units_1 = 5
-lstm_units_2 = 10
+lstm_units_2 = 5
 lstm_layer_num = 2
-learning_rate = 0.0001
+learning_rate = 0.0006
 class_num = 2
 # lstm_time_step = args.lstm_time_step
 step_vector_size = 5
 # step_vector_size = args.step_vector_size
-dropout_ratio = 0.5
+dropout_ratio = 0.8
 # dropout_ratio = args.dropout_ratio
-epochs = 50
+epochs = 300
 # epochs = args.epochs
 
 
@@ -46,8 +46,8 @@ class LSTM4Regression:
         Get X for feature and y for label
         :return: DataFrame of raw data
         """
-        raw_data = util.get_universe_data(self.universe, start_date=self.start_date, end_date=self.end_date)# get raw data
-        X, y = util.feature_label_split(raw_data)
+        raw_data = util.StockRawData.get_universe_data(self.universe, start_date=self.start_date, end_date=self.end_date)# get raw data
+        X, y = util.FatureEngineering.feature_label_split(raw_data)
         return X, y
 
     def get_test_data_fn(self):
@@ -77,38 +77,53 @@ class LSTM4Regression:
         # Build the model
         output_hidden_states, last_states = tf.nn.dynamic_rnn(multi_layer_cell, inputs=X, initial_state=ini_state, time_major=False)
         # Get the hidden state of last time step
-        h_state = tf.nn.softmax(output_hidden_states[, -1, :])
+        h_state = tf.nn.softmax(output_hidden_states[:, -1, :])
         # Set placeholder for W and b for calculating the prediction
         # p_W = tf.placeholder(dtype=tf.float32, shape=[lstm_units_2, class_num], name='out_predict_Weights')
         # p_b = tf.placeholder(dtype=tf.float32, shape=[class_num], name='out_predict_bias')
         # Calculate the y predict
         # y_pre = tf.nn.softmax(tf.matmul())
         # Add a dense layer to calculate the predicted logits
-        logits = tf.layers.Dense(h_state, class_num, name='logits')
+        logits = tf.layers.dense(inputs=h_state, units=class_num, name='dense_1')
         # Add a softmax to calculte the predicted y
-        y_pred = tf.nn.softmax(logits)
-        y_pred = tf.nn.(logits)
+        y_pred = tf.nn.softmax(logits, name='softmax_1')
         return y_pred
 
-    def fit(self, X, y):
+    def fit(self, X_train, y_train):
         """
         Fit feature matrix and label matrxi to the Dual lstm model of tensorflow
         :param X: Featrure matrix
         :param y: Label matrix
         :return: Fitted model
         """
-        X = tf.placeholder(dtype=tf.float32, shape=(-1, lstm_time_step, step_vector_size))
-        y = tf.placeholder(dtype=tf.float32, shape=(-1, 2))
+        X = tf.placeholder(dtype=tf.float32, shape=(batch_size, lstm_time_step, step_vector_size))
+        # print(X.shape)
+        y = tf.placeholder(dtype=tf.float32, shape=(batch_size, class_num))
+        # print(y.shape)
         y_pred = self.__build_model(X)
         # Define the loss
         loss = -tf.reduce_mean(y*tf.log(y_pred))
         train_op = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
+        init_op = tf.initialize_all_variables()
         corrections = tf.equal(tf.argmax(y_pred, 1), tf.argmax(y, 1))
-        accuracy = tf.reduce_mean(tf.cast(corrections), "float")
-
+        accuracy = tf.reduce_mean(tf.cast(corrections, dtype=tf.float32), name="accuracy")
+        batch_num = int(X_train.shape[0]/batch_size)
+        with tf.Session() as sess:
+            sess.run(init_op)
+            for i in range(epochs):
+                for j in range(batch_num):
+                    inp = X_train[j*batch_size:(j+1)*batch_size]
+                    out = y_train[j*batch_size:(j+1)*batch_size]
+                    if (j+1) % 4 == 0:
+                        train_acc = sess.run(accuracy, feed_dict={X: inp, y: out})
+                        print("Epoch - {}, Batch - {}, Train-Acc {}".format(i, j, train_acc))
+                    sess.run(train_op, feed_dict={X: inp, y: out})
+                    print("Traing {}th epoch, {}th batch".format(i, j))
 
 
 if __name__ == "__main__":
-    spe_universe = util.get_universe()
+    spe_universe = util.StockRawData.get_universe()
     strategy = LSTM4Regression(list(spe_universe.code), start_date='2018-01-03', end_date='2018-05-26')
-    a = strategy.build_dataset()
+    X, y = strategy.get_train_data_fn()
+    strategy.fit(X, y)
+
